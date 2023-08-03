e109c715c59f25b899be2e0d9ee54fda5c9e116b
shrikant2753 <shrikantrp.00@gmail.com>
2023-08-03 18:38:21 +0530

    commit

--- home.js
+++ home.js
@@ -42,6 +42,25 @@ async function fetchChannelDetails(channelId = "UC2CxHEcxC9gY70FQe0GhC4Q"){
     return result ;
 }
 
+async function getVideoLikesCount(videoId) {
+//   const baseUrl = "https://www.googleapis.com/youtube/v3/videos";
+  const url = `${baseUrl}/videos?key=${apiKey}&part=statistics&id=${videoId}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.items.length > 0) {
+      const likesCount = data.items[0].statistics.likeCount;
+      return likesCount;
+    } else {
+      console.log("Video not found or statistics not available.");
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
 
 async function addDataOntoUI(videosList) {
     // videosList.forEach((video) => {
@@ -59,7 +78,13 @@ async function addDataOntoUI(videosList) {
         const { snippet } = video;
         let channelId = snippet.channelId;
         const channelDetails = await fetchChannelDetails(channelId);
-        console.log(channelDetails);
+        // console.log(channelDetails);
+            
+        const likeno = await getVideoLikesCount(video.id.videoId)
+        const likeCount = abbreviateNumber(likeno)
+        console.log(channelDetails.items[0].snippet.thumbnails.high.url);
+    
+        let publishTime = getTimeAgo(snippet.publishedAt)
 
         const videoElement = document.createElement("div");
         videoElement.className = "thumbnail-item";
@@ -72,15 +97,15 @@ async function addDataOntoUI(videosList) {
                             </div>
                             <div class="thumbnail-profile-desc">
                                 <div class="t-profile">
-                                    <img src="${channelDetails.items[0].snippet.thumbnails.high}" alt="">
+                                    <img src="${channelDetails.items[0].snippet.thumbnails.high.url}" alt="">
                                 </div>
                                 <div class="description">
                                     <div class="title">${snippet.title}</div>
                                     <div class="channel-info">
                                         <span>${snippet.channelTitle}</span>
                                         <div>
-                                            <span>1.7k</span>
-                                            <span>${snippet.publishedAt}</span>
+                                            <span>${likeCount} Likes</span>
+                                            <span>${publishTime}</span>
                                         </div>
                                     </div>
                                 </div>
@@ -103,3 +128,51 @@ function openVideoPage(videoId) {
     window.location.href = `video.html?videoId=${videoId}`;
 }
 
+
+function getTimeAgo(publishedTime) {
+    const currentTime = new Date();
+    const commentTime = new Date(publishedTime);
+    const diff = currentTime - commentTime;
+    const seconds = Math.floor(diff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const weeks = Math.floor(days / 7);
+    const months = Math.floor(days / 30);
+    const years = Math.floor(days / 365);
+  
+    if (seconds < 60) {
+      return `${seconds} seconds ago`;
+    } else if (minutes < 60) {
+      return `${minutes} minutes ago`;
+    } else if (hours < 24) {
+      return `${hours} hours ago`;
+    } else if (days < 7) {
+      return `${days} days ago`;
+    } else if (weeks < 4) {
+      return `${weeks} weeks ago`;
+    } else if (months < 12) {
+      return `${months} months ago`;
+    } else {
+      return `${years} years ago`;
+    }
+  }
+
+
+  function abbreviateNumber(value) {
+    const suffixes = ["", "k", "M", "B", "T"];
+    const num = parseInt(value);
+    const tier = (Math.log10(num) / 3) | 0;
+  
+    if (tier === 0) return num.toString();
+  
+    const suffix = suffixes[tier];
+    const scale = Math.pow(10, tier * 3);
+    const scaledValue = num / scale;
+  
+    // Limit to 1 decimal place
+    const roundedValue = scaledValue.toFixed(1);
+  
+    return roundedValue + suffix;
+  }
+  
